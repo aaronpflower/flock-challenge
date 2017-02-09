23624be8f6c119332d8648e61e5ab32b12459a91
Aaron Flower <aaronpflower@gmail.com>
2017-02-09 15:55:13 -0700

    updated readme and added calculator

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Flock challenge app
+The purpose of this is to show case some front end delopment skills through 3 different callenges within 2 hours.
+
+First build static todo list with bootstrap 
+
+Second discus the differences between these jQuery functions
+    - $(“.todo-item”).on(“click”, function(e){ console.log(e) });
+    - $(document).on(“click”, “.todo-item”, function(e){ console.log(e) });
+    - Answer: The impact of using the second option over the first is event-delegation. 
+    For example a todo list could contain an unlimited amount of todo-items and following the first pattern would lead to an unlimited about of event listener. 
+    This would become hard to manage as the list grows.  But following the second way the event listener is added just to the document and the on click will just bubble up to the document.
+
+Third build a calculator out of JS that can add, subtract, multiply and divide only numbers.
---
+++ calculator.js
@@ -0,0 +1,58 @@
+var Calculator = {
+    filter: function(...numbers) {
+      return numbers.filter(function(number) {
+          return typeof number !== 'number'
+        })
+    },
+  
+    add: function(...numbers) {
+        var filter = this.filter(...numbers)
+        if (filter.length > 0) {
+            new Error('cant do it')
+        } else {
+            return numbers.reduce(function(number, next) {
+                return number + next
+            })
+        } 
+    },
+    
+    subtract: function(...numbers) {
+        var filter = this.filter(...numbers)
+        if (filter.length > 0) {
+            return new Error('cant do it')
+        } else {
+            return numbers.reduce(function(number, next) {
+                return number - next
+            })
+        } 
+    },
+
+    mulitply: function(...numbers) {
+        var filter = this.filter(...numbers)
+        if (filter.length > 0) {
+            return new Error('cant do it')
+        } else {
+            return numbers.reduce(function(number, next) {
+                return number * next
+            })
+        } 
+    },
+
+    divide: function(...numbers) {
+        var filter = this.filter(...numbers)
+        if (filter.length > 0) {
+            return new Error('cant do it')
+        } else {
+            return numbers.reduce(function(number, next) {
+                return number / next
+            })
+        } 
+    }
+}
+
+// var doThis = Calculator.add(3, 6)
+// var doThis = Calculator.subtract(3, 6)
+// var doThis = Calculator.mulitply(3, 6)
+// var doThis = Calculator.divide(3, 6)
+
+// console.log(doThis)
